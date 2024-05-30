2c6a852397444f34a654e48b882983e8ec380beb
Amin7775 <amin.dpi24@gmail.com>
2024-05-30 19:43:31 +0600

    Add Testimonials With React Rating

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,20 +1,30 @@
 import Banner from "./Banner/Banner";
 import Featured from "./Featured/Featured";
 import PopularMenu from "./PopularMenu/PopularMenu";
+import Testimonials from "./Testimonials/Testimonials";
 import Category from "./category/Category";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            {/* container = max-w-screen-xl mx-auto*/}
-            <section className="max-w-screen-xl mx-auto">
-                <Category></Category>
-                <PopularMenu></PopularMenu>
-            </section>
-                <Featured></Featured>
-        </div>
-    );
+  return (
+    <div className="pb-16">
+      {/* main banner */}
+      <Banner></Banner>
+
+      {/* container = max-w-screen-xl mx-auto*/}
+      <section className="max-w-screen-xl mx-auto">
+        <Category></Category>
+        <PopularMenu></PopularMenu>
+      </section>
+
+      {/* Featured Section */}
+      <Featured></Featured>
+
+      {/* container 2*/}
+      <section className="max-w-screen-xl mx-auto">
+        <Testimonials></Testimonials>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,53 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
+
+// react rating css
+import "@smastrom/react-rating/style.css";
+import { Rating } from "@smastrom/react-rating";
+
+// react icons
+import { FaQuoteLeft } from "react-icons/fa";
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+
+  return (
+    <div className="mb-10">
+      <SectionTitle
+        subHeading={"What Our Clients Say"}
+        heading={"Testimonials"}
+      ></SectionTitle>
+      {/* swiper */}
+      <div className="">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="flex flex-col items-center gap-2 my-5 mx-24">
+                <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+                <FaQuoteLeft className="text-8xl"></FaQuoteLeft>
+                <p>{review.details}</p>
+                <h1 className="text-2xl text-orange-400">{review.name}</h1>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
