8ec3e1346e1bcab7bd199da6fe8059ff1b067fd3
Amin7775 <amin.dpi24@gmail.com>
2024-05-30 16:49:55 +0600

    Use Swiper JS To Create A Category Swiper

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "./Banner/Banner";
+import Category from "./category/Category";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-
+            {/* container = max-w-screen-xl mx-auto*/}
+            <section className="max-w-screen-xl mx-auto">
+                <Category></Category>
+            </section>
         </div>
     );
 };
---
+++ src/Pages/Home/category/Category.jsx
@@ -0,0 +1,55 @@
+import { FreeMode, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// images
+import slide1 from './../../../assets/home/slide1.jpg'
+import slide2 from './../../../assets/home/slide2.jpg'
+import slide3 from './../../../assets/home/slide3.jpg'
+import slide4 from './../../../assets/home/slide4.jpg'
+import slide5 from './../../../assets/home/slide5.jpg'
+
+const Category = () => {
+    return (
+        <div>
+            <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        freeMode={true}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper mb-24"
+      >
+        <SwiperSlide>
+            <img src={slide1} alt="" />
+            <h3 className='text-white text-2xl text-center -mt-16 drop-shadow-lg font-light uppercase'>Salads</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide2} alt="" />
+            <h3 className='text-white text-2xl text-center -mt-16 drop-shadow-lg font-light uppercase'>Pizzas</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide3} alt="" />
+            <h3 className='text-white text-2xl text-center -mt-16 drop-shadow-lg font-light uppercase'>Soups</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide4} alt="" />
+            <h3 className='text-white text-2xl text-center -mt-16 drop-shadow-lg font-light uppercase'>Desserts</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide5} alt="" />
+            <h3 className='text-white text-2xl text-center -mt-16 drop-shadow-lg font-light uppercase'>Special</h3>
+        </SwiperSlide>
+      </Swiper>
+        </div>
+    );
+};
+
+export default Category;
