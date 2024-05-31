30f3cd596d3527f46ec96fda6c89ba64050bde7e
Amin7775 <amin.dpi24@gmail.com>
2024-05-31 18:46:10 +0600

    Setup Recaptcha And Validate Captcha

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,38 @@
 import "./authentication.css";
 import loginImg from './../../assets/others/authentication2.png'
 
+// react simple captcha
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+import { useEffect, useRef, useState } from "react";
+
+
 const Login = () => {
+
+    const [disabled,setDisabled] = useState(true)
+
+    const captchaRef = useRef()
+
+    useEffect(()=>{
+        loadCaptchaEnginge(4); 
+    },[])
+
+    const handleLogin = e =>{
+        e.preventDefault()
+        const form = e.target
+        const email = e.target.email.value
+        const password = e.target.password.value
+        console.log(email,password)
+    }
+
+    const handleValidate = () =>{
+        const user_captcha_value = captchaRef.current.value
+        if(validateCaptcha(user_captcha_value)){
+            setDisabled(false)
+        }else{
+            setDisabled(true)
+        }
+    }
+
   return (
     <div className="min-h-screen background-img p-36 flex">
       <div className="border-4 border-neutral-200 flex-grow drop-shadow-2xl shadow-2xl shadow-neutral-400 p-14 flex items-center">
@@ -12,7 +43,7 @@ const Login = () => {
             </div>
             <div className="card shrink-0 flex-1 ">
             <h1 className="text-4xl font-bold text-center">Login now!</h1>
-              <form className="card-body w-3/4 mx-auto">
+              <form className="card-body w-3/4 mx-auto" onSubmit={handleLogin}>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text font-medium text-lg">Email</span>
@@ -30,6 +61,7 @@ const Login = () => {
                     <span className="label-text font-medium text-lg">Password</span>
                   </label>
                   <input
+                  name="password"
                     type="password"
                     placeholder="password"
                     className="input input-bordered"
@@ -38,32 +70,22 @@ const Login = () => {
                   
                 </div>
                 {/* captcha */}
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text font-medium text-lg">Email</span>
-                  </label>
+                <div className="form-control mt-4">
+                
+                  <LoadCanvasTemplate />
                   <input
-                    type="email"
-                    name="email"
-                    placeholder="email"
-                    className="input input-bordered"
+                  ref={captchaRef}
+                    type="text"
+                    placeholder="Type the captcha above"
+                    className="input input-bordered mt-2"
                     required
                   />
+                <div>
+                    <button onClick={handleValidate} className="btn btn-outline w-full mt-3">Validate Captcha</button>
                 </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text font-medium text-lg">Email</span>
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="email"
-                    className="input input-bordered"
-                    required
-                  />
                 </div>
                 <div className="form-control mt-6">
-                  <button className="btn bg-[#D1A054B3] text-white py-2 h-full text-lg hover:bg-black transition-all duration-300 ease-in-out transform hover:scale-105">Login</button>
+                  <button  disabled={disabled} className="btn bg-neutral-800 text-white py-2 h-full text-lg hover:bg-black transition-all duration-300 ease-in-out transform hover:scale-105">Login</button>
                 </div>
               </form>
             </div>
