5c4be37f6f192ab2e3f31cae1dccad1ff97a7004
Amin7775 <amin.dpi24@gmail.com>
2024-06-12 22:55:06 +0600

    Verify Token And Axios Interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,24 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('access-token')
+        console.log("request stopped by interceptors,", token)
+        config.headers.authorization = `Bearer ${token}`
+        return config;
+    }, function(error){
+        // do something with request error
+        return Promise.reject(error);
+    })
+
+    // intercept 401 and 403
+    axiosSecure.interceptors.response.use(function(response){
+        return response;
+    },(error)=>{
+        const status = error.response.status;
+        console.log("status error in the interceptor", status)
+        return Promise.reject(error)
+    })
     return axiosSecure;
 };
 
