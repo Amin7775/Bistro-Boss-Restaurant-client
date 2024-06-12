e85589c602e6b6d04224ad2d67d1c9284a9475bf
Amin7775 <amin.dpi24@gmail.com>
2024-06-12 14:16:56 +0600

    Cart Data From Custom Hook And Calculate Total Price

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,13 +1,13 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { FaBookmark, FaCartShopping, FaWallet } from "react-icons/fa6";
-import { FaCalendarAlt } from "react-icons/fa";
+import { FaCalendarAlt, FaHome } from "react-icons/fa";
 import { GiStarsStack } from "react-icons/gi";
 import { TbStarsFilled } from "react-icons/tb";
 
 const Dashboard = () => {
   return (
     <div className="flex min-h-full">
-      <div className="w-72 min-h-full bg-orange-400">
+      <div className="w-80 min-h-full bg-orange-400">
         <h1 className="text-3xl font-bold text-wrap px-2 text-center mt-10">Bistro Boss Restaurant</h1>
         <ul className="menu mt-2">
           <li className="px-3 py-2">
@@ -28,7 +28,7 @@ const Dashboard = () => {
           </li>
           <li className="px-3 py-2">
             <NavLink className="flex items-center gap-1" to={"/dashboard/cart"}>
-              <FaCartShopping></FaCartShopping> My Cart
+              <FaCartShopping></FaCartShopping> My Cart 
             </NavLink>
           </li>
           <li className="px-3 py-2">
@@ -41,9 +41,15 @@ const Dashboard = () => {
               <FaBookmark></FaBookmark> My Booking
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li className="px-3 py-2">
+            <NavLink className="flex items-center gap-1" to={"/"}>
+              <FaHome></FaHome> Home
+            </NavLink>
+          </li>
         </ul>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 px-16 ">
         <Outlet></Outlet>
       </div>
     </div>
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,10 +1,75 @@
+import { useEffect } from "react";
+import useCart from "../../../hooks/useCart";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Cart = () => {
-    return (
-        <div>
-            This is cart
-        </div>
-    );
+  const [cart] = useCart();
+
+  //   calculate total price
+  let totalPrice = cart.reduce((sum, index) => {
+    return sum + index.price;
+  }, 0);
+
+  return (
+    <div>
+      {/* page header */}
+      <SectionTitle
+        heading={"Wanna add more ?"}
+        subHeading={"My Cart"}
+      ></SectionTitle>
+      {/* items header */}
+      <div className="flex justify-between mb-5">
+        <h3 className="text-3xl">Items: {cart.length}</h3>
+        <h3 className="text-3xl">Total Price: {totalPrice}</h3>
+        <button className="btn px-10 py-1 bg-neutral-800 text-white text-xl font-normal h-full hover:bg-orange-400">PAY</button>
+      </div>
+      {/* table content */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* rows */}
+            {cart?.map((item,index) => (
+              <tr key={item._id}>
+                <th>
+                  {index+1}
+                </th>
+                {/* image */}
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-14 h-14">
+                        <img
+                          src={item?.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{item?.name}</div>
+                    </div>
+                  </div>
+                </td>
+                {/* price */}
+                <td>{item?.price}</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
