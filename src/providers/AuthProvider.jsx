4fee1f0c7cf56c2152da9df55078ac762049d0d8
Amin7775 <amin.dpi24@gmail.com>
2024-06-12 22:19:36 +0600

    Added header in all users api to send token to backend. Create A JWT Token And Save It On Local Storage

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
 import { app } from "../firebase/firebase.config";
 import { GoogleAuthProvider } from "firebase/auth";
 import { signOut } from "firebase/auth";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 
 export const AuthContext = createContext(null);
@@ -20,6 +21,7 @@ const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic()
 
   // firebase auth related
 
@@ -61,9 +63,16 @@ const AuthProvider = ({ children }) => {
     setUser(currentUser);
     if(currentUser){
       // get token and store it on client
-      
+      const userInfo = {email : currentUser.email}
+      axiosPublic.post('/jwt', userInfo)
+      .then(res=>{
+        if(res.data.token){
+          localStorage.setItem('access-token', res.data.token)
+        }
+      })
     }else{
       // remove token 
+      localStorage.removeItem('access-token')
     }
     setLoading(false);  
     });
