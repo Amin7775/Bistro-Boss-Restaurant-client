3019264f752f5f3f3f99346ce82840f1fbccefa3
Amin7775 <amin.dpi24@gmail.com>
2024-06-16 18:14:26 +0600

    Send Price And Get Client Secret From Stripe

--- src/Pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,26 +1,42 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
 
 const CheckOutForm = () => {
+  const [clientSecret,setClientSecret]=useState('')
   const stripe = useStripe();
   const elements = useElements();
-  const [error, setError] = useState(null);
+  const [error,setError]=useState('')
+  const axiosSecure = useAxiosSecure()
+  const [cart] = useCart()
+  const totalPrice = cart.reduce((total,item) => total + item.price,0)
+
+  useEffect(()=>{
+    axiosSecure.post('/create-payment-intent', {totalPrice})
+    .then(res => {
+      setClientSecret(res.data.clientSecret)
+    })
+  },[axiosSecure,totalPrice])
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
     if (!stripe || !elements) {
       // Stripe.js has not loaded yet. Make sure to disable
       // form submission until Stripe.js has loaded.
       return;
     }
 
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
     const card = elements.getElement(CardElement);
 
     if (card == null) {
       return;
     }
 
+    // Use your card Element with other Stripe.js APIs
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: "card",
       card,
@@ -28,10 +44,10 @@ const CheckOutForm = () => {
 
     if (error) {
       console.log("[error]", error);
-      setError(error.message);
+      setError(error.message)
     } else {
       console.log("[PaymentMethod]", paymentMethod);
-    //   setError(error.message);
+      setError('')
     }
   };
 
@@ -57,8 +73,7 @@ const CheckOutForm = () => {
         <div className="flex justify-center">
           <button
             className="btn mx-auto mt-5 px-10 bg-orange-400 text-black text-lg"
-            type="submit"
-            disabled={!stripe}
+            type="submit" disabled={!stripe || !clientSecret}
           >
             Pay
           </button>
