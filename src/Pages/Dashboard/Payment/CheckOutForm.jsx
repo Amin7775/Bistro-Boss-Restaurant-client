b91dd5fc8b50079f4ef0a5e6e170be9fec01d184
Amin7775 <amin.dpi24@gmail.com>
2024-06-16 18:43:21 +0600

    Use Confirm Card Payment To Process Payment

--- src/Pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -2,6 +2,7 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
+import useAuth from "../../../hooks/useAuth";
 
 const CheckOutForm = () => {
   const [clientSecret,setClientSecret]=useState('')
@@ -11,6 +12,8 @@ const CheckOutForm = () => {
   const axiosSecure = useAxiosSecure()
   const [cart] = useCart()
   const totalPrice = cart.reduce((total,item) => total + item.price,0)
+  const {user} = useAuth()
+  const [transactionId,setTransactionId] = useState('')
 
   useEffect(()=>{
     axiosSecure.post('/create-payment-intent', {totalPrice})
@@ -49,6 +52,27 @@ const CheckOutForm = () => {
       console.log("[PaymentMethod]", paymentMethod);
       setError('')
     }
+
+
+    // confirm payment
+    const {paymentIntent, error: confirmError}= await stripe.confirmCardPayment(clientSecret,{
+      payment_method:{
+        card:card,
+        billing_details:{
+          email: user?.email || 'anonymous',
+          name: user?.displayName || 'anonymous',
+        }
+      }
+    })
+
+    if(confirmError){
+      console.log('confirm error')
+    }else{
+      console.log('payment intent', paymentIntent)
+      if(paymentIntent.status === 'succeeded'){
+        setTransactionId(paymentIntent.id)
+      }
+    }
   };
 
   return (
@@ -79,7 +103,8 @@ const CheckOutForm = () => {
           </button>
         </div>
       </form>
-      {error && <p>{error}</p>}
+      {error && <p className="text-red-500 mt-5 font-medium">{error}</p>}
+      {transactionId && <p className=" mt-5 font-medium">Transaction ID : <span className="text-green-600 font-semibold">{transactionId}</span> </p>}
     </div>
   );
 };
